64bcf4998c764976f9c491d4da24f1593596922e
otmane-elb <otmane.elbaghzaoui@gmail.com>
2023-06-10 19:52:56 +0100

    Seperate the logic and the UI

--- lib/src/features/authentication/screens/on_boarding/on_boarding_screen.dart
+++ lib/src/features/authentication/screens/on_boarding/on_boarding_screen.dart
@@ -1,66 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_rx/src/rx_types/rx_types.dart';
 import 'package:liquid_swipe/liquid_swipe.dart';
 import 'package:login_app/src/constants/colors.dart';
 import 'package:login_app/src/constants/images_strings.dart';
 import 'package:login_app/src/constants/text_strings.dart';
 import 'package:login_app/src/features/authentication/screens/on_boarding/on_boarding_page_widget.dart';
+import 'package:login_app/src/features/controllers/on_boarding_screens_controller.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 import '../../models/on_boarding_screens_model.dart';
 
-class OnBoardingScreen extends StatefulWidget {
+class OnBoardingScreen extends StatelessWidget {
   OnBoardingScreen({super.key});
+  final onboardingcontroller = OnBoardingController();
 
-  @override
-  State<OnBoardingScreen> createState() => _OnBoardingScreenState();
-}
-
-class _OnBoardingScreenState extends State<OnBoardingScreen> {
-  final controller = LiquidController();
-  int currentpage = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(alignment: Alignment.center, children: [
         LiquidSwipe(
-          liquidController: controller,
-          onPageChangeCallback: (activePageIndex) {
-            setState(() {
-              currentpage = activePageIndex;
-            });
-          },
-          pages: [
-            OnBoardingWdiget(
-              model: OnBoardingModel(
-                  image: oOnBoardingImage1,
-                  title: oonboardingpage1text,
-                  counter: '1/3',
-                  bgcolor: oOnBoardingPage1Color),
-            ),
-            OnBoardingWdiget(
-              model: OnBoardingModel(
-                  image: oOnBoardingImage2,
-                  title: oonboardingpage2text,
-                  counter: '2/3',
-                  bgcolor: oOnBoardingPage2Color),
-            ),
-            OnBoardingWdiget(
-              model: OnBoardingModel(
-                  image: oOnBoardingImage3,
-                  title: oonboardingpage3text,
-                  counter: '3/3',
-                  bgcolor: oOnBoardingPage3Color),
-            ),
-          ],
+          liquidController: onboardingcontroller.controller,
+          pages: onboardingcontroller.pages,
+          onPageChangeCallback: onboardingcontroller.onPageChangeCallback,
         ),
-        Positioned(
-            bottom: 10,
-            child: AnimatedSmoothIndicator(
-              activeIndex: controller.currentPage,
-              count: 3,
-              effect: const WormEffect(
-                  dotHeight: 5, activeDotColor: Colors.blueAccent),
-            ))
+        Obx(
+          () => Positioned(
+              bottom: 10,
+              child: AnimatedSmoothIndicator(
+                activeIndex: onboardingcontroller.currentpage.value,
+                count: 3,
+                effect: const WormEffect(
+                    dotHeight: 5, activeDotColor: Colors.blueAccent),
+              )),
+        )
       ]),
     );
   }
--- lib/src/features/controllers/on_boarding_screens_controller.dart
+++ lib/src/features/controllers/on_boarding_screens_controller.dart
@@ -1,5 +1,38 @@
 import 'package:get/get.dart';
+import 'package:liquid_swipe/liquid_swipe.dart';
+import 'package:login_app/src/constants/colors.dart';
+import 'package:login_app/src/constants/images_strings.dart';
+import 'package:login_app/src/constants/text_strings.dart';
+import 'package:login_app/src/features/authentication/models/on_boarding_screens_model.dart';
+import 'package:login_app/src/features/authentication/screens/on_boarding/on_boarding_page_widget.dart';
 
-class OnBoardingController extends GetxController{
-  
+class OnBoardingController extends GetxController {
+  final controller = LiquidController();
+  RxInt currentpage = 0.obs;
+
+  final pages = [
+    OnBoardingWdiget(
+      model: OnBoardingModel(
+          image: oOnBoardingImage1,
+          title: oonboardingpage1text,
+          counter: '1/3',
+          bgcolor: oOnBoardingPage1Color),
+    ),
+    OnBoardingWdiget(
+      model: OnBoardingModel(
+          image: oOnBoardingImage2,
+          title: oonboardingpage2text,
+          counter: '2/3',
+          bgcolor: oOnBoardingPage2Color),
+    ),
+    OnBoardingWdiget(
+      model: OnBoardingModel(
+          image: oOnBoardingImage3,
+          title: oonboardingpage3text,
+          counter: '3/3',
+          bgcolor: oOnBoardingPage3Color),
+    ),
+  ];
+  onPageChangeCallback(int activePageIndex) =>
+      currentpage.value = activePageIndex;
 }
